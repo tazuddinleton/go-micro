8ddd49c8494fc8c6331b9c38b256474705290013
Taz Uddin <getleton@gmail.com>
2023-04-09 00:40:46 +0600

    Add route to handle authenticate request

---
+++ auth-service/cmd/api/hanlders.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+)
+
+func (app *AppConfig) Authenticate(w http.ResponseWriter, r *http.Request) {
+	var reqPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &reqPayload)
+
+	if err != nil {
+		_ = app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	log.Println("request payload: ", reqPayload)
+	user, err := app.Models.User.GetByEmail(reqPayload.Email)
+	if err != nil || user == nil {
+		_ = app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
+
+	log.Println("valid user found: ", user)
+	valid, err := app.Models.User.PasswordMatches(reqPayload.Password)
+
+	if !valid {
+		log.Println("password didn't  match!   ", reqPayload.Password)
+	}
+	if err != nil || !valid {
+		_ = app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	}
+
+	resPayload := jsonResponse{
+		Message: "Authenticated",
+		Error:   false,
+		Data:    user,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, resPayload)
+	if err != nil {
+		return
+	}
+}
---
+++ auth-service/cmd/api/helper.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *AppConfig) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576 // one megabytes
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value.")
+	}
+	return nil
+}
+
+func (app *AppConfig) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	return err
+}
+
+func (app *AppConfig) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+	return app.writeJSON(w, statusCode, payload)
+}
