71b80bf1c3898ffad14e8db60e179f0fc3e3bedb
Taz Uddin <tazuddinleton@gmail.com>
2024-05-02 23:35:12 +0600

    Update

--- auth-service/cmd/api/hanlders.go
+++ auth-service/cmd/api/hanlders.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 )
+
+const loggerServiceUrl = "http://logger-service/log"
 
 func (app *AppConfig) Authenticate(w http.ResponseWriter, r *http.Request) {
 	var reqPayload struct {
@@ -13,7 +18,6 @@ func (app *AppConfig) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err := app.readJSON(w, r, &reqPayload)
-
 	if err != nil {
 		_ = app.errorJSON(w, err, http.StatusBadRequest)
 		return
@@ -38,13 +42,44 @@ func (app *AppConfig) Authenticate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resPayload := jsonResponse{
-		Message: "Authenticated",
+		Message: fmt.Sprintf("Logged in user %s", user.Email),
 		Error:   false,
 		Data:    user,
+	}
+	log.Println("calling logger-service")
+	err = app.logAuthentication("authentication", fmt.Sprintf("Logged in %s", user.Email))
+	if err != nil {
+		log.Println("error calling logger-service")
 	}
 
 	err = app.writeJSON(w, http.StatusOK, resPayload)
 	if err != nil {
 		return
 	}
+}
+
+func (app *AppConfig) logAuthentication(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, err := json.MarshalIndent(entry, "", "/t")
+	if err != nil {
+		return err
+	}
+
+	client := http.Client{}
+	log.Println(string(jsonData))
+	req, err := http.NewRequest(http.MethodPost, loggerServiceUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	_, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
 }
